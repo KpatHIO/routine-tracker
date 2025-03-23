9493c2d7a027f05c79065e42fbfe524ec029cd57
KpatHIO <killiansmith1@gmail.com>
2025-03-23 11:40:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -2,12 +2,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
 import { getFirestore, doc, getDoc, setDoc } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyA4Y9BniC1XCveZ-wBS3BsKvF8VFffXloI",
   authDomain: "routinetracker-3e465.firebaseapp.com",
